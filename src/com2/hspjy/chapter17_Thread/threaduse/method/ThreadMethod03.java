b1f3eb755f2a46e917c5e04fe88424b29c1d5400
Boscoochen <boscochen203@gmail.com>
2022-07-14 15:20:50 -0500

    add setdeamon

--- src/com2/hspjy/chapter17_Thread/threaduse/method/ThreadMethod03.java
+++ src/com2/hspjy/chapter17_Thread/threaduse/method/ThreadMethod03.java
@@ -1,8 +1,14 @@
 package com2.hspjy.chapter17_Thread.threaduse.method;
 
 public class ThreadMethod03 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws InterruptedException {
+        MyDaemonThread myDaemonThread = new MyDaemonThread();
+        myDaemonThread.setDaemon(true);
+        myDaemonThread.start();
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println("宝强在辛苦的工作。。。");
+            Thread.sleep(1000);
+        }
     }
 }
 
@@ -11,11 +17,11 @@ class MyDaemonThread extends Thread {
     public void run() {
         for ( ;; ) {
             try {
-                Thread.sleep(50);
+                Thread.sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            System.out.println("");
+            System.out.println("马蓉和宋喆快乐聊天，哈哈哈～～～");
         }
     }
 }
